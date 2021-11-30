2a12f605717e2b4f3b5ba85d39b5a93dea2a60dc
taiguaramaran <taiguaramaran@gmail.com>
2021-11-30 13:34:25 -0300

    swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://localhost:3000',
+          url: 'https://maino-api-teste.herokuapp.com',
           variables: {
             defaultHost: {
-              default: 'http://localhost:3000'
+              default: 'https://maino-api-teste.herokuapp.com'
             }
           }
         }
